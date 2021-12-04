5d99612fdeaa5715b898c1770d8740952c55633a
travnya <91688913+travnya@users.noreply.github.com>
2021-12-04 19:25:22 +0300

    Add files via upload

---
+++ Sites/Калькулятор/app.js
@@ -0,0 +1,103 @@
+let input = document.querySelector('.inputBox').value
+let operation
+let num1
+
+console.log(15/3)
+
+let add0 = () => {
+  input += 0
+  document.querySelector('.inputBox').value = Number(input)
+}
+
+let add1 = () => {
+  input += 1
+  document.querySelector('.inputBox').value = Number(input)
+}
+
+let add2 = () => {
+  input += 2
+  document.querySelector('.inputBox').value = Number(input)
+}
+
+let add3 = () => {
+  input += 3
+  document.querySelector('.inputBox').value = Number(input)
+}
+
+let add4 = () => {
+  input += 4
+  document.querySelector('.inputBox').value = Number(input)
+}
+
+let add5 = () => {
+  input += 5
+  document.querySelector('.inputBox').value = Number(input)
+}
+
+let add6 = () => {
+  input += 6
+  document.querySelector('.inputBox').value = Number(input)
+}
+
+let add7 = () => {
+  input += 7
+  document.querySelector('.inputBox').value = Number(input)
+}
+
+let add8 = () => {
+  input += 8
+  document.querySelector('.inputBox').value = Number(input)
+}
+
+let add9 = () => {
+  input += 9
+  document.querySelector('.inputBox').value = Number(input)
+}
+
+let addDot = () => {
+  input += '.'
+  document.querySelector('.inputBox').value = Number(input)
+}
+
+let clearBtn = () => {
+  input = ''
+  document.querySelector('.inputBox').value = 0;
+}
+
+let plusBtn = () => {
+  num1 = document.querySelector('.inputBox').value
+  clearBtn()
+  operation = '+'
+}
+
+let minusBtn = () => {
+  num1 = document.querySelector('.inputBox').value
+  clearBtn()
+  operation = '-'
+}
+
+let multiplyBtn = () => {
+  num1 = document.querySelector('.inputBox').value
+  clearBtn()
+  operation = '*'
+}
+
+let delBtn = () => {
+  num1 = document.querySelector('.inputBox').value
+  clearBtn()
+  operation = '/'
+}
+
+let equalBtn = () => {
+  console.log(num1)
+  console.log(input)
+  if (operation == '+') {
+    document.querySelector('.inputBox').value = Number(num1) + Number(input)
+  } else if (operation == '-') {
+    document.querySelector('.inputBox').value = Number(num1) - Number(input)
+  } else if (operation == '*') {
+    document.querySelector('.inputBox').value = Number(num1) * Number(input)
+  } else if (operation == '/') {
+    document.querySelector('.inputBox').value = Number(num1) / Number(input)
+  }
+}
