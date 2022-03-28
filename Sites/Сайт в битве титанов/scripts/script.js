7542456c0cf0156238a31064ad8bb2c5411ea815
vsamsonov <vsamsonov@phoenixit.ru>
2022-03-28 15:05:46 +0300

    Добавил сайт для Титанов

---
+++ Sites/Сайт в битве титанов/scripts/script.js
@@ -0,0 +1,9 @@
+const PopUp = document.getElementById('pop_up');
+
+const show = () => {
+    PopUp.style.visibility = 'visible';
+};
+
+const hide = () => {
+    PopUp.style.visibility = 'hidden';
+};
